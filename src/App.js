412feb6577b85240e30a41295021070258f6542f
iamvijaysubramanian <vijaysubbu69@gmail.com>
2022-10-11 20:33:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./Sidebar";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./css/sb-admin-2.css";
+import "./fontawesome-free/css/all.css";
+import Topbar from "./Topbar";
+import Dashboard from "./Dashboard";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import User from "./User";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" class="d-flex flex-column">
+          <Topbar />
+          <div id="content">
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard/>}></Route>
+              <Route path="/users" element={<User/>}></Route>
+            </Routes>
+            
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function User() {
+  return (
+    <div>
+      <img src="http://forumus-uploads-production.s3-us-west-2.amazonaws.com/original/3X/3/a/3a069c72c7a4dbc768abedfd3e4087604f6162dd.png"></img>
+    </div>
+  );
+}
+
+export default User;
